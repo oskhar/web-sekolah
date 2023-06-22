b471401c1cdf7701778f13eb7016755c1fa83b67
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-06-22 17:08:47 +0700

    Menambahkan navbar pada halaman admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('components.navbarAdmin')
+
+@section('title', 'dashboard')
+
+@section('container')
+    tesdoang
+@endsection
---
+++ resources/views/components/navbarAdmin.blade.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('components.warna')
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="{{asset('css/navbarAdmin.css')}}">
+    <meta charset="utf-8">
+    <link href="assets/images/lightmode.jpg" alt="iniGambar" rel="icon" type="image/x-icon">
+    <title>@yield('title')</title>
+</head>
+<body>
+    <main id="all">
+    <header>
+        <div id="titleHeader">
+            <div><div></div></div>
+            <p>Si Paling Parkir</p>
+        </div>
+        <div id="isiHeader">
+            <div id="identitas">Admin</sup></div>
+            <div id="gridHeader">
+                <button onclick="window.location='bundle.php?page=dashboard'">
+                    <div><svg viewBox="0 0 512 512"><path d="M0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm320 96c0-26.9-16.5-49.9-40-59.3V88c0-13.3-10.7-24-24-24s-24 10.7-24 24V292.7c-23.5 9.5-40 32.5-40 59.3c0 35.3 28.7 64 64 64s64-28.7 64-64zM144 176a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm-16 80a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64zM400 144a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"/></svg></div>
+                    <p>Dashboard</p>
+                </button>
+                <button onclick="window.location='bundle.php?page=grafik'">
+                    <div><svg viewBox="0 0 448 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm64 192c17.7 0 32 14.3 32 32v96c0 17.7-14.3 32-32 32s-32-14.3-32-32V256c0-17.7 14.3-32 32-32zm64-64c0-17.7 14.3-32 32-32s32 14.3 32 32V352c0 17.7-14.3 32-32 32s-32-14.3-32-32V160zM320 288c17.7 0 32 14.3 32 32v32c0 17.7-14.3 32-32 32s-32-14.3-32-32V320c0-17.7 14.3-32 32-32z"/></svg></div>
+                    <p>Grafik</p>
+                </button>
+                <button onclick="window.location='bundle.php?page=akunadmin'">
+                    <div><svg viewBox="0 0 576 512"><path d="M352 144c0-44.2 35.8-80 80-80s80 35.8 80 80v48c0 17.7 14.3 32 32 32s32-14.3 32-32V144C576 64.5 511.5 0 432 0S288 64.5 288 144v48H64c-35.3 0-64 28.7-64 64V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V256c0-35.3-28.7-64-64-64H352V144z"/></svg></div>
+                    <p>Akun Admin</p>
+                </button>
+                <button onclick="window.location='bundle.php?page=platnomor'">
+                    <div><svg viewBox="0 0 576 512"><path d="M64 32C28.7 32 0 60.7 0 96V416c0 35.3 28.7 64 64 64H512c35.3 0 64-28.7 64-64V96c0-35.3-28.7-64-64-64H64zm80 256h64c44.2 0 80 35.8 80 80c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16c0-44.2 35.8-80 80-80zm-32-96a64 64 0 1 1 128 0 64 64 0 1 1 -128 0zm256-32H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H496c8.8 0 16 7.2 16 16s-7.2 16-16 16H368c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/></svg></div>
+                    <p>Plat Nomor</p>
+                </button>
+            </div>
+        </div>
+    </header>
+    <div>
+        <nav>
+            <button onclick="menu()">|||</button>
+            <button onclick="window.location='index.php'">Home</button>
+            <form class="form-search" >
+                <input type="hidden" name="page" value="">
+                <label for="search">
+                    <div class="fancy-bg"></div>
+                    <div class="search">
+                        <svg id="logoSearch" viewBox="0 0 24 24" aria-hidden="true" class="r-14j79pv r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-4wgw6l r-f727ji r-bnwqim r-1plcrui r-lrvibr">
+                            <g>
+                                <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path>
+                            </g>
+                        </svg>
+                        <input class="input" type="text" placeholder="Search" id="search" name="search">
+                    </div> 
+                </label>
+            </form>
+            <div></div>
+            <button onclick="window.location.href='?logout=s'" id="logout"><p>Logout</p></button>
+        </nav>
+        
+        @yield('container')
+
+    </div>
+    </main>
+    <script>
+        var navBuka = true;
+        function menu() {
+            if(navBuka) {
+                document.getElementById('all').classList.remove("bukaNavbar");
+                document.getElementById('all').classList.add("tutupNavbar");
+                navBuka =false;
+            } else {
+                document.getElementById('all').classList.remove("tutupNavbar");
+                document.getElementById('all').classList.add("bukaNavbar");
+                navBuka =true;
+            }
+        }
+    </script>
+</body>
+</html>
