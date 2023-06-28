43e096373a98977377a3105cef5915960bbe56e7
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-06-29 03:20:05 +0700

    Menambahkan navbar utama pada halaman admin

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('components.adminnavbar')
+
+@section('title', "dashboard")
+
+@section('mainContainer')
+    <h1>Tesdoang</h1>
+@endsection
---
+++ resources/views/components/warna.blade.php
@@ -0,0 +1,21 @@
+<style>
+    :root {
+        --w1: #fafafa;
+        --w2: #1c1e26;
+        --w3: #282A36;
+        --w4: #E4E5F1;
+        --w5: #ECAFFF;
+        --darktable: #22242e;
+        --selected: #c072d7;
+        --shadow: rgba(255, 255, 255, 0.5);
+        --danger: #f74373;
+        --warning: #d2a100;
+        --sucess: #23a943;
+    }
+    @keyframes muncul_atas {
+    to {top:0px;}
+    }
+    @keyframes hilang_atas {
+    to {top:-100px;}
+    }
+</style>
