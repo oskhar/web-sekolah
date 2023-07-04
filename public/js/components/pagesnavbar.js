3eef23d9eec5972386c0254a9675c3aec4b4ae84
oskhar <muhamad.oskhar22@mhs.uinjkt.ac.id>
2023-07-04 22:27:44 +0700

    Memperbaiki logic pada navbar

--- public/js/components/pagesnavbar.js
+++ public/js/components/pagesnavbar.js
@@ -1,4 +1,43 @@
-function bukaDropdown(element) {
-    element.classList.toggle("show");
-    // element.querySelector('.dropdown-menu').classList.toggle('show');
-}
+let page_on = false;
+document.querySelector('.dropdown-menu').addEventListener("mouseover", function (params)
+{
+    if (!page_on)
+        document.querySelector('.dropdown-menu').classList.add("show");
+});
+
+document.querySelector('.dropdown-menu').addEventListener("mouseout", function (params)
+{
+    if (!page_on)
+        document.querySelector('.dropdown-menu').classList.remove("show");
+});
+
+document.querySelector('.dropdown-toggle').addEventListener("mouseover", function (params)
+{
+    if (!page_on)
+        document.querySelector('.dropdown-menu').classList.add("show");
+});
+
+document.querySelector('.dropdown-toggle').addEventListener("mouseout", function (params)
+{
+    if (!page_on)
+        document.querySelector('.dropdown-menu').classList.remove("show");
+});
+
+document.querySelector('.dropdown-toggle').addEventListener("click",  (params) =>
+{
+    if (!page_on) {
+        page_on = true;
+        document.querySelector('.dropdown-menu').classList.add("show");
+    } else {
+        page_on = false;
+        document.querySelector('.dropdown-menu').classList.remove("show");
+    }
+});
+
+window.onscroll = () => {
+    if (window.scrollY > 200) {
+        document.querySelector(".navbar").classList.add("nav-fixed");
+    } else {
+        document.querySelector(".navbar").classList.remove("nav-fixed");
+    }
+};
--- resources/views/components/pagesnavbar.blade.php
+++ resources/views/components/pagesnavbar.blade.php
@@ -10,24 +10,17 @@
 <body>
     <section class="ftco-section">
         <div class="container">
-            <div class="row justify-content-center">
-                <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">Website menu #02</h2>
-                </div>
-            </div>
-        </div>
-        <div class="container">
             <div class="row justify-content-between">
                 <div class="col">
-                    <a class="navbar-brand" href="index.html">Papermag <span>Magazine</span></a>
+                    <a class="navbar-brand" href="{{ url('/') }}">Sekolah <span>TK Sejatera</span></a>
                 </div>
                 <div class="col d-flex justify-content-end">
                     <div class="social-media">
                         <p class="mb-0 d-flex">
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-facebook"><i class="sr-only">Facebook</i></span></a>
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-twitter"><i class="sr-only">Twitter</i></span></a>
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-instagram"><i class="sr-only">Instagram</i></span></a>
-                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="fa fa-dribbble"><i class="sr-only">Dribbble</i></span></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="img-facebook"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"/></svg></span></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="img-twitter"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"/></svg></span></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="img-instagram"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224,202.66A53.34,53.34,0,1,0,277.36,256,53.38,53.38,0,0,0,224,202.66Zm124.71-41a54,54,0,0,0-30.41-30.41c-21-8.29-71-6.43-94.3-6.43s-73.25-1.93-94.31,6.43a54,54,0,0,0-30.41,30.41c-8.28,21-6.43,71.05-6.43,94.33S91,329.26,99.32,350.33a54,54,0,0,0,30.41,30.41c21,8.29,71,6.43,94.31,6.43s73.24,1.93,94.3-6.43a54,54,0,0,0,30.41-30.41c8.35-21,6.43-71.05,6.43-94.33S357.1,182.74,348.75,161.67ZM224,338a82,82,0,1,1,82-82A81.9,81.9,0,0,1,224,338Zm85.38-148.3a19.14,19.14,0,1,1,19.13-19.14A19.1,19.1,0,0,1,309.42,189.74ZM400,32H48A48,48,0,0,0,0,80V432a48,48,0,0,0,48,48H400a48,48,0,0,0,48-48V80A48,48,0,0,0,400,32ZM382.88,322c-1.29,25.63-7.14,48.34-25.85,67s-41.4,24.63-67,25.85c-26.41,1.49-105.59,1.49-132,0-25.63-1.29-48.26-7.15-67-25.85s-24.63-41.42-25.85-67c-1.49-26.42-1.49-105.61,0-132,1.29-25.63,7.07-48.34,25.85-67s41.47-24.56,67-25.78c26.41-1.49,105.59-1.49,132,0,25.63,1.29,48.33,7.15,67,25.85s24.63,41.42,25.85,67.05C384.37,216.44,384.37,295.56,382.88,322Z"/></svg></span></a>
+                            <a href="#" class="d-flex align-items-center justify-content-center"><span class="img-dribbble"><i class="sr-only">Dribbble</i></span></a>
                         </p>
                     </div>
                 </div>
@@ -36,18 +29,18 @@
         <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
             <div class="container">
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="fa fa-bars"></span> Menu
+                    <div class="img-bars">|||</div>
                 </button>
                 <form action="#" class="searchform order-lg-last">
                     <div class="form-group d-flex">
                         <input type="text" class="form-control pl-3" placeholder="Search">
-                        <button type="submit" placeholder="" class="form-control search"><span class="fa fa-search"></span></button>
+                        <button type="submit" placeholder="" class="form-control search"><svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M416 208c0 45.9-14.9 88.3-40 122.7L502.6 457.4c12.5 12.5 12.5 32.8 0 45.3s-32.8 12.5-45.3 0L330.7 376c-34.4 25.2-76.8 40-122.7 40C93.1 416 0 322.9 0 208S93.1 0 208 0S416 93.1 416 208zM208 352a144 144 0 1 0 0-288 144 144 0 1 0 0 288z"/></svg></button>
                     </div>
                 </form>
                 <div class="collapse navbar-collapse" id="ftco-nav">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active"><a href="#" class="nav-link">Home</a></li>
-                        <li class="nav-item dropdown" onclick="bukaDropdown(this)">
+                        <li class="nav-item dropdown">
                             <a class="nav-link dropdown-toggle" id="dropdown04" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Page</a>
                             <div class="dropdown-menu" aria-labelledby="dropdown04">
                                 <a class="dropdown-item" href="#">Page 1</a>
@@ -64,6 +57,9 @@
             </div>
         </nav>
     </section>
+    <main style="height: 200vh;background:orange;top:0">
+        Tesdoang
+    </main>
     <script src="{{ asset('js/components/pagesnavbar.js') }}"></script>
 </body>
 </html>
