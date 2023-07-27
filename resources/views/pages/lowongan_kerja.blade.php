0fcd67e234ae3410c4a0429e69ddea2b96d57f8e
haidar halwi <haidarhalwi@gmail.com>
2023-07-27 12:49:24 +0700

    merapihkan code

---
+++ resources/views/pages/lowongan_kerja.blade.php
@@ -0,0 +1,16 @@
+@extends('components.template_pages')
+
+@section('title', "Halaman Contact")
+@section('mainContainer')
+
+<link rel="stylesheet" href="{{ asset('css/pages/kebijakan_privasi.css')}} ">
+
+
+
+
+
+
+
+
+
+@endsection
