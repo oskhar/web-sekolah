231da70a987deab13da1d67801a51d49927473f8
haidar halwi <haidarhalwi@gmail.com>
2023-07-27 12:20:00 +0700

    merapihkan code

--- resources/views/pages/materi.blade.php
+++ resources/views/pages/materi.blade.php
@@ -32,6 +32,27 @@
         </div>
     </div>
 
+    <div class="row row-cols-1 row-cols-md-2 g-4 mt-5">
+        <div class="col">
+            <div class="card">
+                <img src="/assets/images/foto_bareng_tk.jpg" class="img" alt="..." >
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+            </div>
+            <div class="col">
+            <div class="card">
+                <img src="/assets/images/foto_bareng_tk.jpg" class="img" alt="..." >
+                <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
 </div>
 
 
